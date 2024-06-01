d1c18600490c9f7dc4596a8f90faf882a7aca9d2
carlos <cbdev2021@gmail.com>
2024-06-01 03:14:50 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <div>
+
+      </div>
+  
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    reactRefresh()
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://mern-b1-vrcl.vercel.app/',
+        changeOrigin: true,
+      },
+    },
+  },
+})
+ 
