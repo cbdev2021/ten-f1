e9d4cb3852bac06102e7d187eaf8fa0ababb742f
carlos <cbdev2021@gmail.com>
2024-06-01 04:31:13 -0400

    cajas header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,73 +1,26 @@
-import { styled, alpha } from '@mui/material/styles';
+// Header.js
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar'; 
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase'; 
-import SearchIcon from '@mui/icons-material/Search'; 
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-}));
+import Box from '@mui/material/Box';  
 
 const Header = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
-                <Toolbar>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ display: { xs: 'none', sm: 'block' } }}
-                    >
-                        T°en
-                    </Typography>
-                    <Search>
-                        <SearchIconWrapper>
-                            <SearchIcon />
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="Search…"
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </Search>
-                </Toolbar>
+                <div className="header">
+                    <div className="caja caja1">
+                        {/* Contenido de la caja 1 */}
+                    </div>
+                    <div className="col-auto border-right"></div>
+                    <div className="caja caja2">
+                        {/* Contenido de la caja 2 */}
+                    </div>
+                    <div className="caja caja3">
+                        {/* Contenido de la caja 3 */}
+                    </div>
+                    <div className="caja caja4">
+                        {/* Contenido de la caja 4 */}
+                    </div>
+                </div>
             </AppBar>
         </Box>
     );
