52e94fba9acfc5997c803d68054eeaa85aafc73b
carlos <cbdev2021@gmail.com>
2024-06-18 01:46:56 -0400

    nombres componentes

--- src/components/content/Current.tsx
+++ src/components/content/Current.tsx
@@ -1,13 +1,122 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import moment from "moment"; // Importa moment para el manejo de fechas
 
-const Current = () => {
+const Current = ({ current }) => {
+    const { temperature, summary, wind } = current;
+
+    // Formato de fecha
+    // const currentDate = moment().format("DD-MM");
+    const currentDate = moment().format("dddd DD [de] MMMM");
+
     return (
-        <main>
-            <Box className="content">
-                <h1>Current contain </h1>
+        <Box
+            className="content"
+            sx={{
+                maxWidth: 275, // Ancho máximo
+                maxHeight: 400, // Altura máxima
+                borderRadius: 8, // Radio de borde ajustable
+                border: "2px solid #ccc", // Color de borde
+                padding: 2, // Espacio interno
+                background: "linear-gradient(to right, #78a3d1, #dae4e9)",                  
+                // display: "flex",  
+                // flexDirection: "column",  
+                // justifyContent: 'center',                  
+                alignItems: 'center',  
+                marginTop: '90px',
+                marginBottom: '20px'
+            }}
+        >
+            <Typography variant="h2" sx={{ mb: 1, justifyContent:'ce' }}>
+                {temperature.toFixed(0)}°
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 1 }}>
+                {summary}
+            </Typography>
+            <Typography variant="body2" sx={{ mb: 1 }}>
+                {currentDate}
+            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography variant="body2" sx={{ mr: 1 }}>
+                   Wind:  {wind.speed} m/s
+                </Typography>
+                <Typography variant="body2" sx={{ mr: 1 }}>
+                    {wind.angle}°
+                </Typography>
+                <Typography variant="body2">
+                    {wind.dir}
+                </Typography>
             </Box>
-        </main>
+        </Box>
     );
 }
 
 export default Current;
+
+// import { Box, Typography } from "@mui/material";
+// import moment from "moment";
+// import   Draggable   from "react-draggable"; // Importa Draggable desde react-draggable
+
+// const DailyCard = ({ current }) => {
+//     const { temperature, summary, wind } = current;
+
+//     const currentDate = moment().format("dddd DD [de] MMMM");
+
+//     return (
+//         <Draggable>
+//             <Box
+//                 className="content"
+//                 sx={{
+//                     maxWidth: 275,
+//                     maxHeight: 400,
+//                     borderRadius: 8,
+//                     border: "2px solid #ccc",
+//                     padding: 2,
+//                     background: "linear-gradient(to right, #78a3d1, #dae4e9)",
+//                     alignItems: 'center',
+//                     marginTop: '90px',
+//                     marginBottom: '20px',
+//                     cursor: 'move' // Añade cursor: 'move' para indicar que es draggable
+//                 }}
+//             >
+//                 <Typography variant="h2" sx={{ mb: 1 }}>
+//                     {temperature.toFixed(0)}°
+//                 </Typography>
+//                 <Typography variant="body1" sx={{ mb: 1 }}>
+//                     {summary}
+//                 </Typography>
+//                 <Typography variant="body2" sx={{ mb: 1 }}>
+//                     {currentDate}
+//                 </Typography>
+//                 <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+//                     <Typography variant="body2" sx={{ mr: 1 }}>
+//                        Wind:  {wind.speed} m/s
+//                     </Typography>
+//                     <Typography variant="body2" sx={{ mr: 1 }}>
+//                         {wind.angle}°
+//                     </Typography>
+//                     <Typography variant="body2">
+//                         {wind.dir}
+//                     </Typography>
+//                 </Box>
+//             </Box>
+//         </Draggable>
+//     );
+// }
+
+// export default DailyCard;
+
+
+
+// import { Box } from "@mui/material";
+
+// const Current = () => {
+//     return (
+//         <main>
+//             <Box className="content">
+//                 <h1>Current contain </h1>
+//             </Box>
+//         </main>
+//     );
+// }
+
+// export default Current;
